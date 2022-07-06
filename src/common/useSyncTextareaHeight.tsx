7408d2c953cf559bc60877b8b1c584c7ba465dc8
BorislavBranimirov <boriintorr@gmail.com>
2022-07-06 22:59:21 +0300

    bugfix scroll jumping when resizing textarea and an ancestor overflows (is scrollable)

--- src/common/useSyncTextareaHeight.tsx
+++ src/common/useSyncTextareaHeight.tsx
@@ -7,10 +7,21 @@ function useSyncTextareaHeight(body: string) {
     if (inputFieldRef.current) {
       const totalBorderHeight =
         inputFieldRef.current.offsetHeight - inputFieldRef.current.clientHeight;
+
+      const parentEl = inputFieldRef.current.parentElement;
+      if (parentEl) {
+        // preserve height of parent before setting textarea height to 0
+        // prevents scroll jumping in some cases where an ancestor overflows
+        parentEl.style.height = `${parentEl.scrollHeight}px`;
+      }
       inputFieldRef.current.style.height = '0';
 
       inputFieldRef.current.style.height =
         inputFieldRef.current.scrollHeight + totalBorderHeight + 'px';
+      if (parentEl) {
+        // remove the manually set parent height
+        parentEl.style.height = '';
+      }
     }
   }, [body, inputFieldRef]);
 
